f8156f8d28d2c873c2606857d1296b2c2c357f82
Kerry Mwai <kerrywangamati@gmail.com>
2020-12-12 03:11:59 +0300

    lists

--- lib/main.dart
+++ lib/main.dart
@@ -12,23 +12,30 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  
+  List<String> quots=[
+    "Hello programmer 1",
+    "Hello programmer 2",
+    "Hello programmer 3",
+    "Hello programmer 4",
+    "Hello programmer 5",
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title:Text("Awesone Quotes",
         style: TextStyle(
-          color: Colors.blue[900],
+          color: Colors.blue[700],
           letterSpacing: 3.0
         ),
         ),
-        backgroundColor: Colors.redAccent,
+        backgroundColor: Colors.red[100],
         centerTitle: true,
         ),
 
         body: Column(
-
+            children: quots.map((quote)=> Text(quote)
+            ).toList()
         )
       
     );
